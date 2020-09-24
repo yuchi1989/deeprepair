0ebb2dd19a75016b60907dcecac09628719f8dc9
Yuchi <tyc1989711@gmail.com>
2020-09-24 08:06:27 -0400

    add cifar100

--- exp_3/analyze_epoch_confusion.py
+++ exp_3/analyze_epoch_confusion.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+def get_cifar100_labels():
+    import pickle
+    with open("/home/yuchi/Downloads/cifar-100-python/meta", "rb") as metafile:
+        meta = pickle.load(metafile, encoding='latin1')
+    return meta
+
+
 def top_confusions(epoch_confusion_file, n = 5):
     information = np.load(epoch_confusion_file, allow_pickle=True)
     epoch = []
@@ -26,8 +33,8 @@ def top_confusions(epoch_confusion_file, n = 5):
     print(information[max_index]["confusion"][(3, 5)])
     confusion_matrix = information[max_index]["confusion"]
     avg_pair_confusion = {}
-    for i in range(10):
-        for j in range(i + 1, 10):
+    for i in range(100):
+        for j in range(i + 1, 100):
             avg_pair_confusion[(i,j)] = (confusion_matrix[(i, j)] + confusion_matrix[(j, i)])/2
     keys = sorted(avg_pair_confusion, key=avg_pair_confusion.get, reverse=True)[:n]
     for i in range(n):
@@ -40,7 +47,7 @@ def top_confusions(epoch_confusion_file, n = 5):
 
 def draw_graph():
 
-    information = np.load("./log/cifar10_resnet_2_4_epoch_confusion.npy", allow_pickle=True)
+    information = np.load("./log/cifar100_resnet_1_epoch_confusion.npy", allow_pickle=True)
     nature_accuracy = []
     dogcat_confusion = []
     autotruck_confusion = []
@@ -51,9 +58,9 @@ def draw_graph():
         if acc > cur_acc:
             nature_accuracy.append(acc)
             cur_acc = acc
-            dogcat_confusion.append((i["confusion"][(5, 3)] + i["confusion"][(3, 5)])/2)
-            autotruck_confusion.append((i["confusion"][(1, 9)] + i["confusion"][(9, 1)])/2)
-            airship_confusion.append((i["confusion"][(0, 8)] + i["confusion"][(8, 0)])/2)
+            dogcat_confusion.append((i["confusion"][(35, 98)] + i["confusion"][(98, 35)])/2)
+            autotruck_confusion.append((i["confusion"][(47, 52)] + i["confusion"][(52, 47)])/2)
+            airship_confusion.append((i["confusion"][(11, 46)] + i["confusion"][(46, 11)])/2)
     print("first stage")
 
 
@@ -68,16 +75,25 @@ def draw_graph():
     import matplotlib.pyplot as plt
     plt.rcParams.update({'font.size': 12})
     fig, ax = plt.subplots()
-    ax.plot(nature_accuracy, dogcat_confusion, 'r--', label="dog/cat confusion")
-    ax.plot(nature_accuracy, autotruck_confusion, 'g-', label="car/truck confusion")
-    ax.plot(nature_accuracy, airship_confusion, 'b-', label="airplane/ship")
+    ax.plot(nature_accuracy, dogcat_confusion, 'r-', label="girl/woman confusion")
+    ax.plot(nature_accuracy, autotruck_confusion, 'g-', label="maple/oak confusion")
+    ax.plot(nature_accuracy, airship_confusion, 'b-', label="boy/man confusion")
     plt.ylabel("confusion")
     plt.xlabel("natural accuracy")
-    legend = ax.legend(loc='lower left', shadow=True, fontsize=14)
-    plt.savefig("cifar10.pdf", bbox_inches='tight')
+    legend = ax.legend(loc='upper center', shadow=True, fontsize=12)
+    plt.savefig("cifar100.pdf", bbox_inches='tight')
     plt.show()
 
 
 if __name__ == '__main__':
-    #top_confusions("./log/cifar10_resnet_2_4_epoch_confusion.npy", 3)
+    #top_confusions("./log/cifar100_resnet_1_epoch_confusion.npy", 3)
+    '''
+    meta = get_cifar100_labels()
+    print(meta["fine_label_names"][35])
+    print(meta["fine_label_names"][98])
+    print(meta["fine_label_names"][47])
+    print(meta["fine_label_names"][52])
+    print(meta["fine_label_names"][11])
+    print(meta["fine_label_names"][46])
+    '''
     draw_graph()
