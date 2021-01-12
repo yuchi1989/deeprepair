72f7848241526d73d61199869821bbb7e6e5b66f
Yuchi <tyc1989711@gmail.com>
2021-01-12 13:21:31 -0500

    rewrite newbatchnorm

--- exp_7/cifar10/confusion/newbatchnorm2.py
+++ exp_7/cifar10/confusion/newbatchnorm2.py
@@ -34,24 +34,31 @@ class dnnrepair_BatchNorm2d(nn.BatchNorm2d):
                         float(self.num_batches_tracked)
                 else:  # use exponential moving average
                     exponential_average_factor = self.momentum
-
+        e = exponential_average_factor
         # calculate running estimates
         if self.training:
             half_len = input.size(0)//2
             original_half = input[:half_len]
             target_half = input[half_len:]  # target input
-            mean = original_half.mean(
-                [0, 2, 3]) * (1 - self.target_ratio) + target_half.mean([0, 2, 3]) * self.target_ratio
+            target_mean = target_half.mean([0, 2, 3])
             # use biased var in train
-            var = original_half.var([0, 2, 3], unbiased=False) * (1 - self.target_ratio) + \
-                target_half.var([0, 2, 3], unbiased=False) * self.target_ratio
-            n = input.numel() / input.size(1)
+            target_var = target_half.var([0, 2, 3], unbiased=False)
+            n1 = target_half.numel() / target_half.size(1)
+
+            regular_mean = original_half.mean([0, 2, 3])
+            # use biased var in train
+            regular_var = original_half.var([0, 2, 3], unbiased=False)
+            n2 = original_half.numel() / original_half.size(1)
+
             with torch.no_grad():
-                self.running_mean.copy_(exponential_average_factor * mean +
-                                        (1 - exponential_average_factor) * self.running_mean)
-                # update running_var with unbiased var
-                self.running_var.copy_(exponential_average_factor * var * n /
-                                       (n - 1) + (1 - exponential_average_factor) * self.running_var)
+                if self.target_ratio != 0:
+                    self.running_mean.copy_(e * regular_mean + (1-e) * e * target_mean + (1-e) ** 2 * self.running_mean)
+                    # update running_var with unbiased var
+                    self.running_var.copy_(e * n2 / (n2 - 1) * regular_var + (1 - e) * e * n1 / (n1 - 1) * target_var  +  (1 - e) ** 2 * self.running_var)
+                else:
+                    self.running_mean.copy_(e * regular_mean + (1 - e) * self.running_mean)
+                    # update running_var with unbiased var
+                    self.running_var.copy_(e * regular_var * n2 /(n2 - 1) + (1 - e) * self.running_var)
         else:
             mean = self.running_mean
             var = self.running_var
