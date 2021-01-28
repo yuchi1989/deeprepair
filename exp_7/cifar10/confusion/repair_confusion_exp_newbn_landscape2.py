91b665b2a9ab6c83cacc7f096e45ac35ead04a59
Yuchi <tyc1989711@gmail.com>
2021-01-28 12:23:39 -0500

    update

--- exp_7/cifar10/confusion/repair_confusion_exp_newbn_landscape2.py
+++ exp_7/cifar10/confusion/repair_confusion_exp_newbn_landscape2.py
@@ -506,6 +506,7 @@ def train(train_loader, target_train_loader, model, criterion, optimizer, epoch)
         loss2.backward()
         new_input = input - 0.01*input.grad
         print(input.grad)
+        print(input == new_input)
         new_output = model(new_input)
         if args.replace:
             new_loss = criterion(new_output[:new_output.size(
