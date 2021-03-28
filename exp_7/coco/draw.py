a3ea5251e33f1ee9c03f9acbc74676e8e919113c
Yuchi <tyc1989711@gmail.com>
2021-03-28 18:40:17 -0400

    add script

---
+++ exp_7/coco/draw.py
@@ -0,0 +1,22 @@
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--testing_feature_data', type=str, help='testing feature npy file')
+    args = parser.parse_args()
+    feature_data = np.load(args.testing_feature_data)
+    object_list = feature_data["object_list"]
+    person_class = 0
+    for i in range(len(object_list)):
+        if object_list[i] == "person":
+            person_class = i
+            break
+    type2confusion = feature_data["confusion"]
+    for i in range(len(object_list)):
+        print(type2confusion[person_class][i])
+
+
+if __name__ == '__main__':
+    main()
