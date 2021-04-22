9ea03239ebf13aa454799886ecc68644bce0727f
Yuchi <tyc1989711@gmail.com>
2021-04-22 14:53:17 -0400

    cifar100 bias dbr

--- exp_7/cifar100/bias/repair_bias_dbr.py
+++ exp_7/cifar100/bias/repair_bias_dbr.py
@@ -113,6 +113,17 @@ def set_bn_train(module):
     if isinstance(module, torch.nn.modules.batchnorm._BatchNorm):
         module.train()
 
+def compute_confusion(confusion_matrix, first, second):
+    confusion = 0
+    if (first, second) in confusion_matrix:
+        confusion += confusion_matrix[(first, second)]
+    
+    if (second, first) in confusion_matrix:
+        confusion += confusion_matrix[(second, first)]
+    return confusion/2
+
+def compute_bias(confusion_matrix, first, second, third):
+    return abs(compute_confusion(confusion_matrix, first, second) - compute_confusion(confusion_matrix, first, third))
 
 
 def get_dataset_from_specific_classes(target_dataset, first, second, third):
