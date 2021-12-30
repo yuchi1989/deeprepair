352aed9940c0a9a1a9a22d7f7cf43a755d7a6e47
AIasd <zhongziyuan1995@gmail.com>
2021-12-30 14:42:29 -0500

    minor

--- run_script_coco.py
+++ run_script_coco.py
@@ -51,8 +51,8 @@ verbose = ""
 dataset_model_classes = [('coco', 'coco', ("bus", "person", "clock")), ('coco_gender', 'coco_gender', ("handbag", "woman", "man"))]
 tasks = ['confusion', 'bias']
 methods = ['w-aug', 'w-bn', 'w-loss', 'w-dbr']
-params = [0.1]
-epochs = 13
+params = [0.1, 0.3, 0.5, 0.7, 0.9]
+epochs = 18
 
 
 with open('tmp_log_coco.txt', 'w') as f_out:
